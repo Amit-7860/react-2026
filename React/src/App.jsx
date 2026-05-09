9ad038bfde90f39f34102e6d6aaf134c59cf13dc
Amit-7860 <amitamitsahni@gmail.com>
2026-05-09 15:43:35 +0530

    Addind file

---
+++ React/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div id='parent1'>
+      <h1 id='child1'>Hello Guys</h1>
+      <h2 id='child2'>Hello World</h2>
+    </div>
+    <div id='parent2'>
+      <h1 id='child3'>Hello Guys</h1>
+      <h2 id='child4'>Hello World</h2>
+    </div>
+      
+    </>
+  )
+}
+
+export default App
